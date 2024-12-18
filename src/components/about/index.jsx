439415c3c982e551db61abc3ca7023f4580f53a9
Luiz Gabriel Moraes Pinheiro <luizgpinheiro0905@gmail.com>
2024-12-17 23:16:57 -0300

    :heavy_check_mark: feat: Adding images to the About component

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,30 +1,35 @@
 import { useRef, useState } from "react";
 import { useScrollTrigger } from "../../utils/scroll_trigger";
-import { cityJPG, lorem } from "../../assets";
+import { city1JPG, city2JPG, lorem } from "../../assets";
 
 export function About() {
   const sectionRef = useRef(null);
-  const [hasAnimated, setHasAnimated] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
-  useScrollTrigger(
-    sectionRef,
-    () => {
-      if (!hasAnimated) {
-        setHasAnimated(true);
-      }
-    }
-  );
+  useScrollTrigger(sectionRef, () => (setAnimate(true)));
 
   return (
-    <div ref={sectionRef} className="flex lg:flex-row flex-col-reverse justify-center items-center w-full gap-20 min-h-1 m-12">
-
-      <p className="lg:w-1/2 w-5/6 font-Roboto text-center"> 
-        {lorem}
-      </p>
-      { hasAnimated &&
-        <div className="h-60 w-96 bg-green"> 
-          <img src={cityJPG} className=" w-full h-full object-cover lg:mt-10 lg:ml-10 animate-show"/>
-        </div>
+    <div ref={sectionRef} className="flex flex-col items-center w-full gap-10 m-12">
+      <h2 className="text-3xl font-bold font-kanit-thin text-center">Go Anywhere</h2>
+      { animate &&
+        <>
+          <section className="flex lg:flex-row flex-col-reverse justify-center items-center w-full gap-20 min-h-1 animate-show max-w-[1920px]"> 
+            <p className="lg:w-2/5 w-5/6 font-Roboto text-center"> 
+              {lorem}
+            </p>
+            <div className="h-60 w-96 bg-green shadow-2xl rounded-lg">
+              <img src={city1JPG} className=" w-full h-full object-cover lg:mt-10 lg:ml-10 rounded-lg" alt="New York"/>
+            </div>
+          </section>
+          <section className="flex lg:flex-row flex-col justify-center items-center w-full gap-20 min-h-1 animate-show max-w-[1920px]"> 
+            <div className="h-60 w-96 bg-green shadow-2xl rounded-lg">            
+              <img src={city2JPG} className=" w-full h-full object-cover lg:mt-10 lg:ml-10 animate-show rounded-lg" alt="Berlin"/>
+            </div>
+            <p className="lg:w-2/5 w-5/6 font-Roboto text-center"> 
+              {lorem}
+            </p>
+          </section>
+        </>
       }
     </div>
   );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,13 +4,12 @@ import { Products } from '../../components/products';
 
 export function Home() {
   return (
-    <main className="flex flex-col items-center "> 
+    <main className="flex flex-col items-center"> 
       <Hero />
       <div className="flex flex-row justify-center align-middle w-full bg-text border-solid">
         <p className="text-text_secondary lg:text-lg font-Roboto m-1 text-sm">Look the Opportunities, Explore our solutions!</p>
       </div>
       <Products />
-      <h2 className="text-3xl font-bold font-kanit-thin">Go Anywhere</h2>
       <About />
     </main>
   );
