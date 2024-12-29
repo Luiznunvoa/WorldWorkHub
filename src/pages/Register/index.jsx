4c79da0ab985435b160dde9a970f4c23e28dc7cc
Luiz Gabriel Moraes Pinheiro <luizgpinheiro0905@gmail.com>
2024-12-29 19:21:50 -0300

    :heavy_check_mark: feat: Register Prgress Bar

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,27 @@
-export function Register() {
 
+export function Register() {
+  const steps = ["Create Account", "etc", "etc",];
+  
   return (
-  	<main> 
-  		
-  	</main>
+    <main className="flex flex-col items-center w-full p-6">
+      <ul className="flex flex-row max-w-md justify-between mb-6">
+        {steps.map((label, index) => (
+          <li
+            key={index}
+            className="flex flex-row items-center text-sm font-semibold text-center relative text-green-600"
+          >
+          
+            <div className="w-6 h-6 mx-auto rounded-full bg-background_secondary text-white flex items-center justify-center" >
+              {index + 1}
+            </div>
+            {(index + 1) < steps.length &&
+              <div className="w-10 h-4 rounded-lg bg-background_secondary m-5"> 
+                
+              </div>
+            }
+          </li>
+        ))}                
+      </ul>
+    </main>
   );
 }
