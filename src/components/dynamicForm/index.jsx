2935fb9b30bbafe09c7f7065d4bfd86afc2fa950
Luiz Gabriel Moraes Pinheiro <luizgpinheiro0905@gmail.com>
2025-01-01 15:51:17 -0300

    :heavy_check_mark: feat: DynamicForm Component

---
+++ src/components/dynamicForm/index.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import PropTypes from "prop-types";
+
+export function DynamicForm({ steps }) {
+  const [step, setStep] = useState(0);
+  const methods = useForm();
+
+  const nextStep = async () => {
+    const isValid = await methods.trigger(); // Valida todos os campos da etapa atual
+    if (isValid) {
+      setStep((prev) => (prev < steps.length - 1 ? prev + 1 : prev));
+    }
+  };
+
+  const previousStep = () => setStep((prev) => (prev > 0 ? prev - 1 : prev));
+
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("Form submitted!");
+  };
+
+  function ErrorMessage({ error }) {
+    return error ? <p className="text-red-500 text-sm">{error}</p> : null;
+  }
+
+  ErrorMessage.propTypes = {
+    error: PropTypes.string,
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <ul className="mb-6 flex flex-row justify-between">
+        {steps.map((_, index) => (
+          <li
+            key={index}
+            className="text-green-600 relative flex flex-row items-center text-center text-sm font-semibold"
+          >
+            <div
+              className={`mx-auto h-12 w-12 rounded-xl ${index <= step
+                ? "bg-green text-text"
+                : "bg-background_secondary text-text_secondary"
+              } flex items-center justify-center text-xl`}
+            >
+              {index + 1}
+            </div>
+            {index + 1 < steps.length && (
+              <div
+                className={`h-6 w-32 rounded-lg ${index < step ? "bg-green" : "bg-background_secondary"
+                } m-5`}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+      <FormProvider {...methods}>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className="flex w-[32rem] flex-col items-center rounded-lg bg-white p-6 shadow"
+        >
+          <h2 className="m-5 font-Roboto text-lg font-bold">{steps[step].title}</h2>
+          {steps[step].inputs.map((input, index) => (
+            <div key={index} className="w-full mb-4">
+              <input
+                {...methods.register(input.name, {
+                  required: input.required,
+                  minLength: input.minLength,
+                  pattern: input.pattern,
+                  ...(input.validate && {
+                    validate: (value) =>
+                      value === methods.getValues(input.validate) ||
+											`${input.validate} do not match`,
+                  }),
+                })}
+                type={input.type}
+                placeholder={input.placeHolder}
+                className="w-full rounded border border-gray-300 p-2"
+              />
+              <ErrorMessage error={methods.formState.errors[input.name]?.message} />
+            </div>
+          ))}
+          <div className="flex flex-row items-center justify-center gap-5">
+            {step > 0 && (
+              <button
+                type="button"
+                onClick={previousStep}
+                className="mt-4 rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
+              >
+								Previous
+              </button>
+            )}
+            {step < steps.length - 1 && (
+              <button
+                type="button"
+                onClick={nextStep}
+                className="mt-4 rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
+              >
+								Next
+              </button>
+            )}
+            {step === steps.length - 1 && (
+              <button
+                type="submit"
+                className="mt-4 rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
+              >
+								Submit
+              </button>
+            )}
+          </div>
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
+
+DynamicForm.propTypes = {
+  steps: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      inputs: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          required: PropTypes.string,
+          validate: PropTypes.string,
+          minLength: PropTypes.shape({
+            value: PropTypes.number.isRequired,
+            message: PropTypes.string.isRequired,
+          }),
+          pattern: PropTypes.shape({
+            value: PropTypes.instanceOf(RegExp).isRequired,
+            message: PropTypes.string.isRequired,
+          }),
+          type: PropTypes.string.isRequired,
+          placeHolder: PropTypes.string,
+        })
+      ).isRequired,
+    }).isRequired
+  ).isRequired,
+};
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,208 +1,112 @@
-import { useState } from "react";
-import { useForm, FormProvider } from "react-hook-form";
+import { DynamicForm } from "../../components/dynamicForm";
 
 export function Register() {
-  const [step, setStep] = useState(0);
-  const steps = ["Account Information", "Personal Details", "Preferences"];
-  const methods = useForm();
-
-  const nextStep = () =>
-    setStep((prev) => (prev < steps.length - 1 ? prev + 1 : prev));
-  const previousStep = () => setStep((prev) => (prev > 0 ? prev - 1 : prev));
-
-  const onSubmit = (data) => {
-    console.log(data);
-    alert("Form submitted!");
-  };
 
   return (
     <main className="flex w-full flex-col items-center p-6">
-      <ul className="mb-6 flex flex-row justify-between">
-        {steps.map((label, index) => (
-          <li
-            key={index}
-            className="text-green-600 relative flex flex-row items-center text-center text-sm font-semibold"
-          >
-            <div
-              className={`mx-auto h-12 w-12 rounded-xl ${index <= step ? "bg-green text-text" : "bg-background_secondary text-text_secondary"} flex items-center justify-center text-xl`}
-            >
-              {index + 1}
-            </div>
-            {index + 1 < steps.length && (
-              <div
-                className={`h-6 w-32 rounded-lg ${index < step ? "bg-green" : "bg-background_secondary"} m-5`}
-              />
-            )}
-          </li>
-        ))}
-      </ul>
-      <h1 className="m-5 font-kanit-thin text-2xl font-bold italic">
-        {steps[step]}
-      </h1>
-
-      <FormProvider {...methods}>
-        <form
-          onSubmit={methods.handleSubmit(onSubmit)}
-          className="flex w-[32rem] flex-col items-center rounded-lg bg-white p-6 shadow"
-        >
-          {step == 0 && (
-            <>
-              <h2 className="m-5 font-Roboto text-lg font-bold">
-                Create your account
-              </h2>
-              <input
-                {...methods.register("username", {
-                  required: "username is required",
-                })}
-                type="name"
-                placeholder="Name"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("email", {
-                  required: "Email is required",
-                })}
-                type="email"
-                placeholder="Email"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("password", {
-                  required: "Password is required",
-                })}
-                type="password"
-                placeholder="Password"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("confirmPassword", {
-                  required: "Confirm your password",
-                })}
-                type="password"
-                placeholder="Confirm Password"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-            </>
-          )}
-
-          {step == 1 && (
-            <>
-              <h2 className="m-5 font-Roboto text-lg font-bold">
-                Personal Details
-              </h2>
-              <input
-                {...methods.register("occupation", {
-                  required: "Occupation is required",
-                })}
-                type="text"
-                placeholder="Occupation"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("address", {
-                  required: "Address is required",
-                })}
-                type="text"
-                placeholder="Address"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("cpf", { required: "CPF is required" })}
-                type="text"
-                placeholder="CPF"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <select
-                {...methods.register("education", {
-                  required: "Education level is required",
-                })}
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              >
-                <option value="">Select Education Level</option>
-                <option value="highschool">High School</option>
-                <option value="bachelor">Bachelors Degree</option>
-                <option value="master">Masters Degree</option>
-                <option value="phd">PhD</option>
-              </select>
-              <input
-                {...methods.register("languages", {
-                  required: "Languages are required",
-                })}
-                type="text"
-                placeholder="Languages spoken"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <input
-                {...methods.register("phone", {
-                  required: "Phone number is required",
-                })}
-                type="tel"
-                placeholder="Phone Number"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-            </>
-          )}
-
-          {step == 2 && (
-            <>
-              <h2 className="m-5 font-Roboto text-lg font-bold">Preferences</h2>
-              <textarea
-                {...methods.register("hobbies")}
-                placeholder="Describe your hobbies"
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              />
-              <label className="mb-4 flex items-center">
-                <input
-                  {...methods.register("notifications")}
-                  type="checkbox"
-                  className="mr-2"
-                />
-                Receive notifications
-              </label>
-              <select
-                {...methods.register("communicationPreference", {
-                  required: "Please select a communication preference",
-                })}
-                className="mb-4 w-full rounded border border-gray-300 p-2"
-              >
-                <option value="">Preferred Communication Format</option>
-                <option value="email">Email</option>
-                <option value="phone">Phone</option>
-                <option value="text">Text Message</option>
-              </select>
-            </>
-          )}
-
-          <div className="flex flex-row items-center justify-center gap-5">
-            {step > 0 && (
-              <button
-                type="button"
-                onClick={previousStep}
-                className="rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
-              >
-                Previous
-              </button>
-            )}
-            {step < steps.length - 1 && (
-              <button
-                type="button"
-                onClick={nextStep}
-                className="rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
-              >
-                Next
-              </button>
-            )}
-            {step == steps.length - 1 && (
-              <button
-                type="submit"
-                className="rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500"
-              >
-                Submit
-              </button>
-            )}
-          </div>
-        </form>
-      </FormProvider>
+      <DynamicForm
+        steps={[
+          {
+            title: "Create your account",
+            inputs: [
+              {
+                name: "firstname",
+                type: "text",
+                required: "First Name is required",
+                minLength: {
+                  value: 2,
+                  message: "First name must have at least 2 characters",
+                },
+                pattern: {
+                  value: /^[a-zA-Z]+$/,
+                  message: "First name can only contain letters",
+                },
+                placeHolder: "First Name",
+              },
+              {
+                name: "lastname",
+                type: "text",
+                required: "Last Name is required",
+                minLength: {
+                  value: 2,
+                  message: "Last name must have at least 2 characters",
+                },
+                pattern: {
+                  value: /^[a-zA-Z]+$/,
+                  message: "Last name can only contain letters",
+                },
+                placeHolder: "Last Name",
+              },
+              {
+                name: "email",
+                type: "email",
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Please enter a valid email address",
+                },
+                placeHolder: "Email",
+              },
+              {
+                name: "password",
+                type: "password",
+                required: "Password is required",
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                  message:
+                    "Password must be at least 8 characters long and contain both letters and numbers",
+                },
+                placeHolder: "Password",
+              },
+              {
+                name: "confirmPassword",
+                type: "password",
+                required: "Confirm your password",
+                validate: "password",
+                placeHolder: "Confirm Password",
+              },
+            ],
+          },
+          {
+            title: "Address Information",
+            inputs: [
+              {
+                name: "address",
+                type: "text",
+                required: "Address is required",
+                placeHolder: "Address",
+              },
+              {
+                name: "city",
+                type: "text",
+                required: "City is required",
+                placeHolder: "City",
+              },
+              {
+                name: "zipcode",
+                type: "text",
+                required: "Zip Code is required",
+                pattern: {
+                  value: /^\d{5}(-\d{4})?$/,
+                  message: "Please enter a valid zip code",
+                },
+                placeHolder: "Zip Code",
+              },
+            ],
+          },
+          {
+            title: "Confirmation",
+            inputs: [
+              {
+                name: "terms",
+                type: "checkbox",
+                required: "You must accept the terms and conditions",
+                placeHolder: "I accept the terms and conditions",
+              },
+            ],
+          },
+        ]} 
+      />
     </main>
   );
 }
